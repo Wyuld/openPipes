9bd6f6581dcb855606eb21d00dda98bec3d05cbf
RafaelLdaSilva <rlslink@hotmail.com>
2025-08-28 00:14:06 -0300

    Mais melhorias no installer.sh

--- installer.sh
+++ installer.sh
@@ -5,6 +5,9 @@ amass_path=$(which amass)
 dnsrecon_ver="https://github.com/darkoperator/dnsrecon/archive/refs/tags/1.1.3.zip"
 dnsrecon_dir=$(readlink -f $(which dnsrecon) | rev | sed 's-^[^/]*--' | rev)
 path="$(which ls | rev | sed 's-^[^/]*--' | rev)"
+scripts_dir="$HOME/.openpipes/scripts/"
+
+echo $path
 
 echo "##############################################################"
 echo "  Copying .openpipes folder to your \$HOME ... "
@@ -18,13 +21,32 @@ echo "  Copying scripts to folder $path ... "
 echo "  Please enter sudo credentials ... "
 echo "##############################################################"
 
-sudo cp $HOME/.openpipes/scripts/*.sh $path
+sudo cp $scripts_dir/* $path
 
 echo "##############################################################"
-echo "  Fixing amass and dnsrecon versions ... "
+echo "  Making scripts executable (chmod +x) ... "
+echo "##############################################################"
+
+for file in $(ls $scripts_dir | rev | cut -d "/" -f1 | rev);do 
+    sudo chmod +x $path/$file;
+done
+
+echo "##############################################################"
+echo "  Downgrading amass to version 3.20.0 ... "
 echo "##############################################################"
 
 wget $amass_ver && unzip amass_linux_amd64.zip && sudo cp amass_linux_amd64/amass $amass_path
-wget $dnsrecon_ver && unzip 1.1.3.zip && sudo rm -rf $dnsrecon_dir && sudo mkdir -p $dnsrecon_dir && sudo cp -r dnsrecon-1.1.3/* $dnsrecon_dir
+
+echo "##############################################################"
+echo "  Downgrading dnsrecon to version 1.1.3 ... "
+echo "##############################################################"
+
+wget $dnsrecon_ver && unzip 1.1.3.zip && sudo rm -rf $dnsrecon_dir/* && sudo mkdir -p $dnsrecon_dir && sudo cp -r dnsrecon-1.1.3/* $dnsrecon_dir
+
+echo "##############################################################"
+echo "  Installing RDAP ... "
+echo "##############################################################"
+
+sudo apt-get update && sudo apt install rdap
 
 # dependencies=("")
