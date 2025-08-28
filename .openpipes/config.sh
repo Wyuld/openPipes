d51a8ffdef1167798bfd989e64636b817a479224
RafaelLdaSilva <rlslink@hotmail.com>
2025-08-28 03:03:34 -0300

    Mais melhorias no installer.sh. Melhorias de maneira geral...

--- .openpipes/config.sh
+++ .openpipes/config.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-proj=""
+proj_dir=""
+proj_name=""
+proj_path="$proj_dir/$proj_name"
 obsdir="$HOME/.obsidianFixedMount/"
 tpdir="$HOME/.openpipes/.templates/"
-base_dir="$proj/Varreduras"
+base_dir="$proj_path/Varreduras"
 
 securitytrailskey=""
+OPENAI_API_KEY=""
