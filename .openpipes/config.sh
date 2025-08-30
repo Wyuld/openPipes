4e60e332a790e6d4a9301c2c33e7eb394fcfb07c
RafaelLdaSilva <rlslink@hotmail.com>
2025-08-30 09:47:03 -0300

    ...

--- .openpipes/config.sh
+++ .openpipes/config.sh
@@ -5,7 +5,7 @@ proj_name=""
 proj_path="$proj_dir/$proj_name"
 obsdir="$HOME/.obsidianFixedMount/"
 tpdir="$HOME/.openpipes/.templates/"
-base_dir="$proj_path/Varreduras"
+base_dir="$proj_path/Varreduras/"
 
 securitytrailskey=""
 OPENAI_API_KEY=""
